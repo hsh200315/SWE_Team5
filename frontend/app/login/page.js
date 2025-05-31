b44708aadb3bae6b875ca4c549deabf2b82b0ed5
hsh <hsh200315@naver.com>
2025-05-31 11:54:42 +0900

    로그인 요청 및 username 세션에 저장

--- frontend/app/login/page.js
+++ frontend/app/login/page.js
@@ -6,7 +6,7 @@ import { useRouter } from 'next/navigation';
 
 export default function Home() {
     const router = useRouter();
-    const [username, setUsername] = useState('');
+    const [username, setUsername] = useState('Sena');
 
     const OnClickEvent = async() => {
         if (!username.trim()) {

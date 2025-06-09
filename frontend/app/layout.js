ce8d584d205e025926921f15ea2ca1838ec43b73
jihun3666 <jihun3666@gmail.com>
2025-06-09 11:49:09 +0900

    Roboto 글꼴 추가

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -3,8 +3,15 @@ import "./globals.css";
 export default function RootLayout({ children }) {
   return (
     <html lang="kr">
+      <head>
+        <link
+        href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;700&display=swap"
+        rel="stylesheet"
+        />
+      </head>
       <body
         className={`antialiased`}
+        style={{fontFamily: "Roboto, sans-serif"}}
       >
         {children}
       </body>

4fd2b1341b97822daa028ce3ae7260f755941bcc
jihun3666 <jihun3666@gmail.com>
2025-06-14 18:37:57 +0900

    친구 초대 버튼 검정색으로 변경

--- frontend/app/components/Sidebar.js
+++ frontend/app/components/Sidebar.js
@@ -148,7 +148,7 @@ export default function Sidebar({
                       SetInvitedUsers([]);
                       SetModalOnOff(true);
                     }}
-                    className="block w-full text-left px-4 py-2 hover:bg-gray-100"
+                    className="block w-full text-left px-4 py-2 text-black hover:bg-gray-100"
                   >
                     친구 초대
                   </button>
